fbe8bb91c8d651a44f518975ca4a645d10b3b301
蒋焰 <846814595@qq.com>
2018-06-07 11:52:11 +0800

    阿里 规范

--- auth-user/src/main/java/cn/kr/authuser/service/UserService.java
+++ auth-user/src/main/java/cn/kr/authuser/service/UserService.java
@@ -4,13 +4,32 @@ import cn.kr.authuser.domain.User;
 
 import java.util.List;
 
+/**
+ * 用户service
+ * @author Administrator
+ */
 public interface UserService {
 
 
+    /**
+     * 用户注册
+     * @param username 用户名
+     * @param password 密码
+     */
     void register(String username, String password);
 
+    /**
+     * 根据 用户名和密码 查询 用户
+     * @param username 用户名
+     * @param password 密码
+     * @return
+     */
     User findOne(String username, String password);
 
+    /**
+     * 查询所有用户
+     * @return
+     */
     List<User> findAll();
 
 }
